591e7dc40a9ccf1c6468b0efc68652e9830f9a47
JiangDamen <124563041+JiangDamen@users.noreply.github.com>
2023-02-09 17:04:22 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,27 @@
+$('.menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
+  });
+  
+  $('.nav a').on('click', function () {
+    $('.menu').removeClass('active');
+    $('.overlay').removeClass('menu-open');
+  });
+  
+  document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+  function ctrlShiftKey(e, keyCode) {
+    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+  }
+  
+  document.onkeydown = (e) => {
+    // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+    if (
+      event.keyCode === 123 ||
+      ctrlShiftKey(e, 'I') ||
+      ctrlShiftKey(e, 'J') ||
+      ctrlShiftKey(e, 'C') ||
+      (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    )
+      return false;
+  };
